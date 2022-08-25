171ebe75db61b2d377c4e59341383c08f338f4cb
shlheidari <soheilheidari289@yahoo.com>
2022-08-25 22:03:13 +0430

    log in

--- myworld/register/urls.py
+++ myworld/register/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('signup/addrecord/', views.addrecord, name='addrecord'),
+    path('login/loging/', views.loging, name='loging'),
+    path('loged_in/', views.loged_in, name='loged_in'),
 ]
--- myworld/register/views.py
+++ myworld/register/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .models import Members
+from django.contrib import messages
 
 def index(request):
   template = loader.get_template('choose.html')
@@ -28,3 +29,26 @@ def addrecord(request):
   member = Members(first_name = first_name, last_name = last_name, user_name = user_name, password = password, ssn = ssn, email = email, phone = phone, security_question = security_question, answer = answer)
   member.save()
   return HttpResponseRedirect(reverse('index'))
+
+def loging(request):
+    user_name = request.POST['user_name']
+    password = request.POST['password']
+    try:
+        global member
+        member = Members.objects.get(user_name=user_name)
+        if password == member.password:
+            return HttpResponseRedirect(reverse('loged_in'))
+        else:
+            messages.add_message(request, messages.ERROR, 'your username and password didnt match')
+            return HttpResponseRedirect(reverse('login'))
+    except:
+        messages.add_message(request, messages.ERROR, 'please enter your username and password correctly')
+        return HttpResponseRedirect(reverse('login'))
+
+def loged_in(request):
+    global member
+    context = {
+      'first_name' : member.first_name , 'last_name' : member.last_name, 'user_name' : member.user_name, 'password' : member.password, 'ssn': member.ssn, 'email' : member.email, 'phone' : member.phone, 'security_question' : member.security_question, 'answer' : member.answer
+    }
+    template = loader.get_template('loged_in.html')
+    return HttpResponse(template.render(context,request))
