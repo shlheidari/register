57b826ee82ad17c7807a310f8a06df55edbe7276
shlheidari <soheilheidari289@yahoo.com>
2022-08-25 21:02:42 +0430

    adding 5 members

--- myworld/register/models.py
+++ myworld/register/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Members(models.Model):
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  user_name = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
+  phone = models.CharField(max_length=255)
+  security_question = models.CharField(max_length=255)
+  answer = models.CharField(max_length=255)
+  ssn = models.CharField(max_length=255)
+  password = models.CharField(max_length=255)
